bb8c94b02c34f73ddc5e778e188518627b2cd9b4
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-17 17:17:10 -0230

    Basic cities class created.

---
+++ src/main/java/cities/Cities.java
@@ -0,0 +1,40 @@
+package cities;
+
+public class Cities {
+    private int id;
+    private String name;
+    private String state;
+    private int population;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
