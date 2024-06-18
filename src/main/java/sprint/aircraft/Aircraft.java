ff7c43a246bfe3a6d02f8651b6df23658419dd29
Ks.chris352010 <136537957+ks-chris352010@users.noreply.github.com>
2024-06-18 01:57:08 -0230

    + Added classes
    - Passengers (id, firstName, lastName, phoneNumber)
    - Airports (id, name, code)
    - Aircraft (id, type, airlineName, numberOfPassengers)

---
+++ src/main/java/sprint/aircraft/Aircraft.java
@@ -0,0 +1,40 @@
+package sprint.aircraft;
+
+public class Aircraft {
+    private int id;
+    private String type;
+    private String airlineName;
+    private int numberOfPassengers;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+}
---
+++ src/main/java/sprint/passengers/Passengers.java
@@ -0,0 +1,40 @@
+package sprint.passengers;
+
+public class Passengers {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
